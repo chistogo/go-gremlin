6de4cf4566bad763a9053cadaf002cd13b947deb
Taylor <fierywood@gmail.com>
2016-03-31 22:07:31 -0500

    Switched to UDP. But Not Getting an AWK From Gremlin and unable to send files at this time.

--- client.go
+++ client.go
@@ -8,56 +8,95 @@ import (
     "os/exec"
     "os"
     "io"
+    "time"
     // "bufio"
 )
 
 
 func main() {
     
-    gremlinIP := "127.0.0.1"
-    gremlinPort := "8888"
+    // gremlinIP := "127.0.0.1"
+    // gremlinPort := "8002"
     
     clear()
     fmt.Println("ʕ◔ϖ◔ʔ  Welcome to the GO Gremlin Client Process!!!  ʕ◔ϖ◔ʔ")
     
     //Open File
     file, err := os.Open("go.png")
-    testFile, _ := os.Create("testfile.png.")
+
+    
+    checkError(err)   
+   
+    //Listen for Grimlin for dropped Packets
+    // GremlinAddr, err := net.ResolveUDPAddr("udp",":9001") //Creates the Grimlim Address Object for listening
+    // checkError(err)
+    // ListeningConn, err := net.ListenUDP("udp", GremlinAddr) //Creates the connection for listening from the Grimlim
+    // checkError(err)
+    
+    //Create Sending Object
+    serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8001") //Address to Send to
+    checkError(err)
+    localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0") //LocalAddress
+    checkError(err)
+    
+    //Create the Connection for Sending 
+    SendingConn, err := net.DialUDP("udp", localAddr, serverAddr)
+    checkError(err)
+    //defer SendingConn.Close()
     
     
+    
+    fmt.Println("Sending File ...")
+    //Sends to file to Gremlin
+    //Copy over the data. DataReceive <-- DataSend    
     //Making 32 byte buffer
     buff := make([]byte , 32)
     
     
     var offSet int64
+    var sequenceNum byte;
+    sequenceNum = 0
+    seqBuff := make([]byte, 1)
     offSet = 0
+    
     for{
-        _, err := file.ReadAt(buff, offSet)
-        testFile.Write(buff)
+             
+        numberOfBytes, err := file.ReadAt(buff, offSet) // Read file with our buffer
+        SendingConn.Write(append(buff[:numberOfBytes], sequenceNum)) // Places sequenceNum at the end
+        
        if (err == io.EOF) {
            break;
        }else{
            checkError(err)
        }
+       //Time for timeout
+       t:=time.Now()
+       SendingConn.SetDeadline(t.Add(time.Second))
+       //Recieves ACK packet
+       _,_,err = SendingConn.ReadFromUDP(seqBuff)
+       //If time out occures
+       
+       if(err.(net.Error).Timeout()){
+            SendingConn.Write(append(buff[:numberOfBytes], sequenceNum))
+            fmt.Println("Resending")
+       }else{
+       checkError(err)
+       
+       
        offSet = offSet + 32
+       
+       if(sequenceNum == 255){ //If the sequenceNum is at its max value restart
+            sequenceNum = 0
+        } else {
+            sequenceNum++
+        }
+       }//end of else
+        
+       
     }
-    
-    
-    
-    
-    
-    
-    checkError(err)   
-   
-    //Connect to Gremlin
-    conn, err := net.Dial("tcp", gremlinIP+":"+gremlinPort)
+  
     checkError(err)
-    fmt.Println("Sending File ...")
-    //Sends to file to Gremlin
-    //Copy over the data. DataReceive <-- DataSend    
-    status, err := io.Copy(conn,file)
-    checkError(err)
-    fmt.Println(status)
+    
     fmt.Println("Done Sending")
    
 }
--- gremlin.go
+++ gremlin.go
@@ -8,8 +8,9 @@ import (
     "os/exec"
     //"os"
     // "io"
-    "io/ioutil"
+   // "io/ioutil"
     // "bytes"
+    "os"
 )
 
 
@@ -17,58 +18,92 @@ func main(){
     clear()
     fmt.Println("ʕ◔ϖ◔ʔ  Welcome to the GO Gremlin, Gremlin Process!!!  ʕ◔ϖ◔ʔ")
     
-    gremlinPort := "8888"
+    //Receive from sender port
     
-    ln, err := net.Listen("tcp", ":"+gremlinPort)
+    clientAddr , err := net.ResolveUDPAddr("udp","127.0.0.1:8001")
+    checkError(err)
     
-    if err != nil {
-        // handle error
-    }
+    clientConn, err := net.ListenUDP("udp", clientAddr)
+    checkError(err)
     
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            // handle error
+    testFile , err := os.Create("dump.png")
+    checkError(err)
+    
+    seqBuff := make([]byte, 1)
+    buf := make([]byte, 33)
+    var currentSequenceNum byte
+    currentSequenceNum = 0
+    var oldSequenceNum byte 
+    oldSequenceNum = 0 
+    
+    for{
+        numOfBytesReceived, _ , err := clientConn.ReadFromUDP(buf)
+        checkError(err)
+        //The sequenceNum on the packet matches the anticipated sequenceNum
+        fmt.Printf("Current SeqNum:%d\n",buf[numOfBytesReceived -1])
+        if(currentSequenceNum == buf[numOfBytesReceived -1]) {
+            _, err = testFile.Write(buf[:numOfBytesReceived-1])
+            seqBuff[0] = currentSequenceNum 
+            clientConn.Write(seqBuff)
+            checkError(err)
+            oldSequenceNum = currentSequenceNum
+            if(currentSequenceNum == 255){
+                currentSequenceNum = 0
+            }else{
+                currentSequenceNum++
+                fmt.Println(currentSequenceNum)
+            }       
+        //Client didn't get AWK of the old packet
+        } else if (buf[numOfBytesReceived -1] == oldSequenceNum){
+            seqBuff[0] = oldSequenceNum
+            clientConn.Write(seqBuff)
+            
+        }else {
+            
         }
-        go handleConnection(conn)
+        fmt.Printf("Received %d bytes\n", numOfBytesReceived)
+        
+        
+        checkError(err)
+        
     }
 
 }
 
 
 //TODO: Make sure this is READ and WRITTEN using a buffer
-func handleConnection(conn net.Conn){
+// func handleConnection(conn net.Conn){
     
-    receiverIP := "127.0.0.1"
-    receiverPort := "8887"
-    connToReceive, err := net.Dial("tcp", receiverIP+":"+receiverPort)
-    checkError(err)
-    fmt.Println("Sending File ...")
+//     receiverIP := "127.0.0.1"
+//     receiverPort := "8887"
+//     connToReceive, err := net.Dial("tcp", receiverIP+":"+receiverPort)
+//     checkError(err)
+//     fmt.Println("Sending File ...")
     
-    //file, err := os.Create("newfile.png")
+//     //file, err := os.Create("newfile.png")
     
     
-    //We can change the buffer size if needed
-   // buff := make([]byte, 32) //Creates a buffer for 64 Bytes
-    reader, err := ioutil.ReadAll(conn)
-    checkError(err)
-    //writer := 
+//     //We can change the buffer size if needed
+//    // buff := make([]byte, 32) //Creates a buffer for 64 Bytes
+//     reader, err := ioutil.ReadAll(conn)
+//     checkError(err)
+//     //writer := 
         
-    fmt.Println("Connection:"+conn.RemoteAddr().String())
+//     fmt.Println("Connection:"+conn.RemoteAddr().String())
     
-    //  Newfile<==OLD
-    //conn.Write
-    for i := 0;  i < len(reader); i++ { //while i is less than the length of the reader
-        if(i % 32 ==  0 && i != 0){     //If 
-               connToReceive.Write(reader[i-32:i])
-        }
-        if(i % 32 ==  0 && i + 32 > len(reader)){
-            connToReceive.Write(reader[i:len(reader)])
-            connToReceive.Close()
-        }
-    }
+//     //  Newfile<==OLD
+//     //conn.Write
+// //     for i := 0;  i < len(reader); i++ { //while i is less than the length of the reader
+// //         if(i % 32 ==  0 && i != 0){     //If 
+// //                connToReceive.Write(reader[i-32:i])
+// //         }
+// //         if(i % 32 ==  0 && i + 32 > len(reader)){
+// //             connToReceive.Write(reader[i:len(reader)])
+// //             connToReceive.Close()
+// //         }
+// //     }
     
-}
+// // }
 
 
 
